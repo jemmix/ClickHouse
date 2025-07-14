e2a5b4778447cf5a5c2c4b29655276200d451ebe
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-07-14 15:58:00 +0000

    Generate lambdas elsewhere

--- src/Client/BuzzHouse/Generator/SQLExpression.cpp
+++ src/Client/BuzzHouse/Generator/SQLExpression.cpp
@@ -964,8 +964,10 @@ void StatementGenerator::generateExpression(RandomGenerator & rg, Expr * expr)
     const uint32_t table_star_expr = 10
         * static_cast<uint32_t>((this->allow_not_deterministic || this->levels[this->current_level].inside_aggregate)
                                 && std::find_if(level_rels.begin(), level_rels.end(), has_rel_name_lambda) != level_rels.end());
+    const uint32_t lambda_expr = 3 * static_cast<uint32_t>(this->fc.max_depth > this->depth && this->fc.max_width > this->width);
     const uint32_t prob_space = literal_value + col_ref_expr + predicate_expr + cast_expr + unary_expr + interval_expr + columns_expr
-        + cond_expr + case_expr + subquery_expr + binary_expr + array_tuple_expr + func_expr + window_func_expr + table_star_expr;
+        + cond_expr + case_expr + subquery_expr + binary_expr + array_tuple_expr + func_expr + window_func_expr + table_star_expr
+        + lambda_expr;
     std::uniform_int_distribution<uint32_t> next_dist(1, prob_space);
     const uint32_t noption = next_dist(rg.generator);
 
@@ -1262,6 +1264,18 @@ void StatementGenerator::generateExpression(RandomGenerator & rg, Expr * expr)
             }
         }
         expr->mutable_comp_expr()->mutable_table()->set_table(rg.pickRandomly(filtered_relations).get().name);
+    }
+    else if (
+        lambda_expr
+        && noption
+            < (literal_value + col_ref_expr + predicate_expr + cast_expr + unary_expr + interval_expr + columns_expr + cond_expr + case_expr
+               + subquery_expr + binary_expr + array_tuple_expr + func_expr + window_func_expr + table_star_expr + lambda_expr + 1))
+    {
+        const uint32_t nexprs = std::min(this->fc.max_width - this->width, rg.nextMediumNumber() % 4);
+
+        this->depth++;
+        generateLambdaCall(rg, nexprs, expr->mutable_comp_expr()->mutable_lambda());
+        this->depth--;
     }
     else
     {
