bebe9f9d2d386c70e7629c1c0f4df13e64d7107a
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-07-14 07:11:36 +0200

    Style check pass 1

--- src/Client/ClientBaseHelpers.cpp
+++ src/Client/ClientBaseHelpers.cpp
@@ -101,9 +101,9 @@ bool isChineseNewYearMode(const String & local_tz)
     for (auto d : chineseNewYearIndicators)
     {
         /// Let's celebrate until Lantern Festival
-        if (std::cmp_less_equal(d , days) && d + 25 >= days)
+        if (std::cmp_less_equal(d, days) && d + 25 >= days)
             return true;
-        if (std::cmp_greater(d , days))
+        if (std::cmp_greater(d, days))
             return false;
     }
     return false;
